ad83dd71237bbfb23836d37450e0591f819661a3
mattcoulter711 <matthew@kynection.com>
2021-10-04 17:39:20 +1100

    Distance text is updating now

--- Testing/Assets/UI/UIUpdater.cs
+++ Testing/Assets/UI/UIUpdater.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIUpdater : MonoBehaviour
 {
     public Distance distance;
-    public TextMesh text;
+    public TextMeshProUGUI text;
 
     // Update is called once per frame
     void Update()
     {
-        text.text = distance.GetDistance().ToString();
+        float dist = distance.GetDistance();
+        dist = Mathf.Round(dist);
+        text.SetText(dist.ToString());
     }
 }
