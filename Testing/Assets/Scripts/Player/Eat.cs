763294497ec4b5f2ebc6462b0fb7f7ba46f75a67
mattcoulter711 <matthew@kynection.com>
2021-10-06 21:24:09 +1100

    Reorganised some folders

---
+++ Testing/Assets/Scripts/Player/Eat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatHoney : MonoBehaviour
+{
+    public GameObject HoneyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Testing/Assets/Scripts/Player/SlowDown.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : MonoBehaviour
+{
+    public GameObject slowDownPrefab; // object which slows the player down
+    public float slowDownFactor = 0.5f;
+}
